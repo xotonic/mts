e45c3c618a1b2f94c0b80a4062fa6ab138cc3ef9
xotonic <xotonic@yandex.ru>
2019-10-02 00:25:20 +0200

    Make initializaytion of database connection lazy

--- src/main/java/com/revolut/mts/H2Database.java
+++ src/main/java/com/revolut/mts/H2Database.java
@@ -15,27 +15,25 @@ public class H2Database {
     H2Database() {
         // Enable this to make H2 recognize files in resources directory
         ClassPathURLSupport.enable();
-        initializeDataSource();
     }
 
-    private void initializeDataSource() {
-        try {
-            var properties = new Properties();
-            properties.load(getClass().getResourceAsStream("/config.properties"));
-            dataSource = new JdbcDataSource();
-            dataSource.setURL(properties.getProperty("db.url"));
-            dataSource.setUser(properties.getProperty("db.username"));
-            dataSource.setPassword(properties.getProperty("db.password"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private void initializeDataSource() throws IOException {
+        var properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/config.properties"));
+        dataSource = new JdbcDataSource();
+        dataSource.setURL(properties.getProperty("db.url"));
+        dataSource.setUser(properties.getProperty("db.username"));
+        dataSource.setPassword(properties.getProperty("db.password"));
     }
 
 
     public Connection connection() {
         try {
+            if (dataSource == null) {
+                initializeDataSource();
+            }
             return dataSource.getConnection();
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
     }
